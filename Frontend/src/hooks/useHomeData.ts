7f0f74afdbc19d031482412b25d9a4599c19ba16
Andre Rodrigues <andreprodrigues0512@gmail.com>
2025-10-08 10:50:52 +0100

    Update hook

--- Frontend/src/hooks/useHomeData.ts
+++ Frontend/src/hooks/useHomeData.ts
@@ -1,10 +1,12 @@
 // src/hooks/useHomeData.ts
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useContext } from "react";
 import { getUserHabits } from "../services/habitService";
 import { completeHabit } from "../services/userService";
 import { useXpAchievements } from "./useXpAchievements";
+import { AuthContext } from "../contexts/AuthContext";
 
 export function useHomeData(user: any) {
+    const { setUser } = useContext(AuthContext);
     const [habits, setHabits] = useState<any[]>([]);
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [refreshing, setRefreshing] = useState(false);
@@ -46,16 +48,6 @@ export function useHomeData(user: any) {
             const response = await completeHabit(user.token, habitId);
             const updatedHabit = response.habit;
             setHabits(prev => prev.map(h => (h._id === habitId ? updatedHabit : h)));
-
-
-            // Atualiza XP e nível imediatamente no contexto
-            if (response.newLevel != null) {
-                setUser(prev => ({
-                    ...prev,
-                    currentXp: (prev.currentXp ?? 0) + response.xpGained,
-                    level: response.newLevel,
-                }));
-            }
 
             // Update XP/Level
             await fetchXpAchievements();
